a2cc306dfe92439080bbee8bf512084d0d598fcc
Ronnie Valenzuela <onie.valenzuelaaa02@gmail.com>
2024-04-22 23:40:37 +0800

    completed

--- main.js
+++ main.js
@@ -17,77 +17,154 @@ navLinks.addEventListener("click", (e) =>{
 })
 
 const scrollRevealOption = {
-    distance: "50px",
+    distance: "75px",
     origin: "bottom",
     duration: 1000,
 };
 
-// header container
-ScrollReveal().reveal(".header-image img", {
+ScrollReveal().reveal(".nav-header .nav-logo", {
     ...scrollRevealOption,
+    delay: 500,
+    origin: "right",
+});
+
+ScrollReveal().reveal("nav .nav-links li", {
+    ...scrollRevealOption,
+    delay: 750,
+    origin: "right",
+    interval: "200",
+});
+
+ScrollReveal().reveal(".header-container .header-image img", {
+    ...scrollRevealOption,
+    delay: 1000,
+    origin: "top",
+    distance: "100px",
 });
 
 ScrollReveal().reveal(".header-content .section-header", {
     ...scrollRevealOption,
-    delay: 500,
+    delay: 1250,
+    origin: "left",
+    distance: "100px",
 });
 
 ScrollReveal().reveal(".header-content p", {
     ...scrollRevealOption,
-    delay: 1000,
+    delay: 1350,
+    origin: "left",
+    distance: "100px",
 });
 
-ScrollReveal().reveal(".header-btn", {
+ScrollReveal().reveal(".header-content .header-btn", {
     ...scrollRevealOption,
-    delay: 1500,
+    delay: 1450,
+    origin: "left",
+    distance: "100px",
 });
 
-// about container
-ScrollReveal().reveal(".about-image img", {
+ScrollReveal().reveal(".about-container .about-image img", {
+    ...scrollRevealOption,
+    delay: 1550,
+    origin: "top",
+    distance: "100px",
+});
+
+ScrollReveal().reveal(".about-container .about-content .section-header ", {
+    ...scrollRevealOption,
+    delay: 1650,
+    origin: "right",
+    distance: "100px",
+});
+
+ScrollReveal().reveal(".about-container .section-description", {
     ...scrollRevealOption,
     delay: 1750,
+    origin: "right",
+    distance: "100px",
 });
 
-ScrollReveal().reveal(".about-content h1", {
-    ...scrollRevealOption,
-    delay: 2000,
-    origin: "left",
-});
-
-ScrollReveal().reveal(".about-content .section-description", {
-    ...scrollRevealOption,
-    delay: 2250,
-    origin: "bottom",
-});
-
-// LAST CONTAINER
 ScrollReveal().reveal(".last-container .section-header", {
     ...scrollRevealOption,
-    delay: 2500,
+    delay: 1850,
     origin: "top",
-});
-
-ScrollReveal().reveal(".last-container .blue-text", {
-    ...scrollRevealOption,
-    delay: 2750,
-    origin: "left",
+    distance: "100px",
 });
 
 ScrollReveal().reveal(".last-container .blue-text-last", {
     ...scrollRevealOption,
-    delay: 3000,
+    delay: 1950,
     origin: "left",
+    distance: "100px",
 });
 
 ScrollReveal().reveal(".last-container .red-text-last", {
     ...scrollRevealOption,
-    delay: 3250,
+    delay: 2050,
     origin: "right",
+    distance: "100px",
 });
 
-ScrollReveal().reveal(".last-container .image-and-text", {
+ScrollReveal().reveal(".container-images .image-and-text", {
     ...scrollRevealOption,
-    delay: 3500,
-    origin: "right",
-    interval: "400",
+    delay: 2150,
+    origin: "top",
+    distance: "100px",
+    interval: "200",
 });
+
+ScrollReveal().reveal(".footer .footer-col ", {
+    ...scrollRevealOption,
+    delay: 2250,
+    origin: "top",
+    distance: "100px",
+    interval: "200",
+});
+
+ScrollReveal().reveal(".footer .footer-bar", {
+    ...scrollRevealOption,
+    delay: 2300,
+    origin: "bottom",
+    distance: "100px",
+});
+
+// Scroll
+
+//scoll up
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, the scroll up will appear 
+    if (window.scrollY >= 350) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+};
+
+window.addEventListener('scroll', scrollUp);
+
+// Cursor
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function(e){
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    // cursorOutline.style.left = `${posX}px`;
+    // cursorOutline.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`,
+
+    }, {duration: 500, fill: "forwards"})
+})
+
+
+
+
+
